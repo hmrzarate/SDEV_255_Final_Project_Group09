25bed0f112098063f2cdebeeb5185b25bbce5b79
hmrzarate <112135561+hmrzarate@users.noreply.github.com>
2023-02-22 20:35:09 -0500

    Add files via upload

---
+++ teacherlist.js
@@ -0,0 +1,7 @@
+var teacherNames = [
+    {firstName : "John", lastName : "Doe"},
+    {firstName : "Jacob", lastName : "Smith"},
+    {firstName : "Mary", lastName: "Williams"},
+    {firstName : "Jessica", lastName: "Lewis"},
+    {firstName : "Rachel", lastName: "Parker"}
+];
